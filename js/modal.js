596696f96f19f24a4951b2f291ba825c8c7afbc5
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-19 11:35:02 +0300

    hideModal on cancel btn

--- js/modal.js
+++ js/modal.js
@@ -4,14 +4,22 @@ console.log('modal.js file was loaded');
 // taikomes
 const els = {
   showBnt: document.getElementById('show'),
+  cancelBnt: document.getElementById('cancel'),
   modalEl: document.getElementById('modal'),
 };
 
 // event listeners
-
+els.showBnt.addEventListener('click', showModal);
+els.cancelBnt.addEventListener('click', hideModal);
 // functions
 
 // showModal() - nuimti d-none nuo modalo
 function showModal() {
+  console.log('showing modal');
   els.modalEl.classList.remove('d-none');
+}
+// hideModal() - uzdeti d-none ant modalo
+function hideModal() {
+  console.log('hiding modal');
+  els.modalEl.classList.add('d-none');
 }
