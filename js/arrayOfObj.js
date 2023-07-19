6217eafbed2ae7504c223e78cb0d245d029dc1b7
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-19 10:48:07 +0300

    changeIncomeToSalary

--- js/arrayOfObj.js
+++ js/arrayOfObj.js
@@ -88,4 +88,56 @@ const driversGendersArr = people
   .filter((x) => x.hasCar === true)
   .map((pObj) => pObj.sex);
 
-console.log('driversGendersArr ===', driversGendersArr);
+// console.log('driversGendersArr ===', driversGendersArr);
+
+/* 8. Pagal people masyvą, naudojant Array.prototype.map, suformuokite masyva pakeičiant savybę 'income' į 'salary'
+   pirma sukuriam nauja savybe i objekta 'salary'
+   istrinti income */
+/* 
+org
+{
+  name: 'Jonas',
+  surname: 'Jonaitis',
+  sex: 'male',
+  age: 26,
+  income: 1200,
+  married: false,
+  hasCar: false,
+},
+rez
+{
+  name: 'Jonas',
+  surname: 'Jonaitis',
+  sex: 'male',
+  age: 26,
+  married: false,
+  hasCar: false,
+  salary: 1200,
+},
+*/
+function changeIncomeToSalary() {
+  // sukti cikla
+  const salarayArr = people.map((pObj) => {
+    // a. ciklo metu grazinti nauja norima objekta
+    const naujasObj = {
+      name: pObj.name,
+      surname: pObj.surname,
+      sex: pObj.sex,
+      age: pObj.age,
+      married: pObj.married,
+      hasCar: pObj.hasCar,
+      salary: pObj.income,
+    };
+    return naujasObj;
+  });
+  console.table(salarayArr);
+  // b. ciklo metu nusikopijuoti pObj
+  // atlikti copijai pakeitimus
+  // grazinti kopija
+
+  // grazinti pakeista masyva
+  return salarayArr;
+}
+const graza1 = changeIncomeToSalary();
+console.log('graza1 ===', graza1);
+console.table(people);
